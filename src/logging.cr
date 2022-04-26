ac3a2da2a36c5d4302bd7408c70a4b718184bfb4
Caspian Baska <email@caspian.computer>
2022-04-26 21:34:22 +1000

    feat(logging): add configuration by LOG_LEVEL env var

--- src/logging.cr
+++ src/logging.cr
@@ -10,13 +10,19 @@ module App::Logging
   log_level = App.running_in_production? ? ::Log::Severity::Info : ::Log::Severity::Debug
   namespaces = ["action-controller.*", "place_os.*", "#{App::APP_NAME}.*"]
 
-  ::Log.setup do |config|
-    config.bind "*", :warn, log_backend
+  builder = ::Log.builder
+  builder.bind "*", log_level, log_backend
 
-    namespaces.each do |namespace|
-      config.bind namespace, log_level, log_backend
-    end
+  namespaces.each do |namespace|
+    builder.bind namespace, log_level, log_backend
   end
+
+  ::Log.setup_from_env(
+    default_level: log_level,
+    builder: builder,
+    backend: log_backend,
+    log_level_env: "LOG_LEVEL",
+  )
 
   PlaceOS::LogBackend.register_severity_switch_signals(
     production: App.running_in_production?,
