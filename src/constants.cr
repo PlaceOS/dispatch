a1dc7a85f7872cbb68a1bb25b1bbdd4916738cf7
Caspian Baska <email@caspian.computer>
2022-04-27 20:09:42 +1000

    feat(logging): configure OpenTelemetry

--- src/constants.cr
+++ src/constants.cr
@@ -2,7 +2,7 @@ require "action-controller/logger"
 require "secrets-env"
 
 module App
-  APP_NAME     = "Dispatch"
+  APP_NAME     = "dispatch"
   VERSION      = {{ `shards version "#{__DIR__}"`.chomp.stringify.downcase }}
   BUILD_TIME   = {{ system("date -u").stringify }}
   BUILD_COMMIT = {{ env("PLACE_COMMIT") || "DEV" }}
